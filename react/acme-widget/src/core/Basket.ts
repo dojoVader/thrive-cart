564c049f3be8284195582ebafc2a71ec1ad52244
dojoVader <okeowoaderemi@gmail.com>
2026-08-20 18:39:37 +0100

    feat: Introduce ProductList component and refactor Basket class for improved offer handling

--- react/acme-widget/src/core/Basket.ts
+++ react/acme-widget/src/core/Basket.ts
@@ -2,31 +2,27 @@ import type { Product } from './Product';
 import type { IDeliveryRule } from './rules/IDeliveryRule';
 import type { IOffer } from './offer/IOffer';
 
-/**
- * The Basket Class is responsible handling the logic of handling product code, delivery charges, offers and total costs
- * , it's built to factor in Delivery Charges applied to the total cost of the items in the basket
- * and also apply any offers that may be available for the products in the basket
- */
+
 export class Basket {
     private product_code: string[] = [];
 
     /**
      * The Product Catalogue array
      */
-    private product_catalogues: Record<string, Product>;
+    private readonly product_catalogues: Record<string, Product>;
 
     private deliveryRule: IDeliveryRule;
 
-    private offers: IOffer[];
+    private readonly offers: IOffer[];
 
     public constructor(
         product_catalogue: Record<string, Product>,
         deliveryRuleCharges: IDeliveryRule,
-        offers: IOffer[],
+        offers?: IOffer[],
     ) {
         this.product_catalogues = product_catalogue;
         this.deliveryRule = deliveryRuleCharges;
-        this.offers = offers;
+        this.offers = offers || [];
     }
 
     public add(product_code: string): void {
@@ -50,7 +46,7 @@ export class Basket {
             offerApplied = offer.apply(this.product_code, this.product_catalogues);
         }
 
-        total_sum -= offerApplied;
+        total_sum = total_sum - offerApplied;
 
         const totalCharges = this.deliveryRule.calculate(total_sum);
 
